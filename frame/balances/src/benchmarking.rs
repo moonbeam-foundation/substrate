67ed582b653dbe1653fc8119468c20f4b59d2a45
Nisheeth Barthwal <nbaztec@gmail.com>
2023-08-18 14:43:08 +0200

    minimize changes

--- frame/balances/src/benchmarking.rs
+++ frame/balances/src/benchmarking.rs
@@ -46,14 +46,14 @@ mod benchmarks {
 		let caller = whitelisted_caller();
 
 		// Give some multiple of the existential deposit
-		let balance = existential_deposit;
+		let balance = existential_deposit.saturating_mul(ED_MULTIPLIER.into());
 		let _ = <Balances<T, I> as Currency<_>>::make_free_balance_be(&caller, balance);
 
 		// Transfer `e - 1` existential deposits + 1 unit, which guarantees to create one account,
 		// and reap this user.
 		let recipient: T::AccountId = account("recipient", 0, SEED);
 		let recipient_lookup = T::Lookup::unlookup(recipient.clone());
-		let transfer_amount = existential_deposit;
+		let transfer_amount = balance;
 
 		#[extrinsic_call]
 		_(RawOrigin::Signed(caller.clone()), recipient_lookup, transfer_amount);
@@ -153,7 +153,7 @@ mod benchmarks {
 		let source_lookup = T::Lookup::unlookup(source.clone());
 
 		// Give some multiple of the existential deposit
-		let balance = existential_deposit;
+		let balance = existential_deposit.saturating_mul(ED_MULTIPLIER.into());
 		let _ = <Balances<T, I> as Currency<_>>::make_free_balance_be(&source, balance);
 
 		// Transfer `e - 1` existential deposits + 1 unit, which guarantees to create one account,
